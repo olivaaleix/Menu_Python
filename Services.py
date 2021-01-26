c522b3683a3f9c096f43d0bd99bb91b12c16d850
Pau Camós <45793792+paucamos@users.noreply.github.com>
2021-01-26 22:23:17 +0100

    Updated pid function

--- Services.py
+++ Services.py
@@ -1,6 +1,12 @@
 #   Menu of PID Windows
 
 import psutil
+import os
+import sys
+
+def get_pid(name):
+    notepads = (item.pid for item in psutil.process_iter() if item.name() == name)
+    return list(notepads)
 
 def KeyNum():
 
@@ -14,6 +20,7 @@ def KeyNum():
             1.- Check and prints the actual PID and process names
             2.- Option 2
             3.- Option 3
+            0.- Close
         ''')
 
         try:
@@ -32,20 +39,27 @@ while not exit:
     option = KeyNum()
 
     if option == 1:
-        print ("PID Windows :") #This 1st option lists with name and PID the actual active services
-
-        for proc in psutil.process_iter():
-            try: 
-              pinfo = proc.as_dict (attrs=["pid", "name"])
-            except psutil.NoSuchProcess:
-              pass
+        pid_name = input("PID Windows: ") #This 1st option lists with name and PID the actual active services
+        results = get_pid(pid_name + ".exe")
+        
+        if len(results) > 0:
+            # close them
+            print(results)
+            print("\n")
+            print("Do you want to terminate the process? (y/n)")
+            confirm = input()
+            if confirm.lower() == 'y':
+                os.system("taskkill /im "+pid_name+".exe 2> nul")
         else:
-             print(pinfo)
+            print('No items found with that PID.')
 
     elif option == 2:
         print ("Option 2")
     elif option == 3:
         exit = True
+    elif option == 0:
+        print("Thanks for using this program!")
+        sys.exit()
     else:
         print(" Give me a correct option")
 
